be8cdacc0155e77fb5f855e975a8aaaae0914df3
Berkay Uysak <77589774+uysak@users.noreply.github.com>
2022-09-18 23:28:14 +0300

    Created new controller classes

---
+++ WebAPI/Controllers/RentalController.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalController : ControllerBase
+    {
+        private IRentalService _rentalService;
+        public RentalController(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+        [HttpGet]
+        public IActionResult GetRentals()
+        {
+            var result = _rentalService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("newrent")]
+        public IActionResult Rent(Rental rental)
+        {
+            var result = _rentalService.Rent(rental);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("returncar")]
+        public IActionResult ReturnCar(Rental rental)
+        {
+            var result = _rentalService.ReturnCar(rental);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
